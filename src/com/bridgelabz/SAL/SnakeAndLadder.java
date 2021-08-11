c369457e0a8a8e54746ea7ec399e199866f56073
Prakash-16 <goudprakash58@gmail.com>
2021-08-11 10:32:47 +0530

    [Prakash] - Added wining condition

--- src/com/bridgelabz/SAL/SnakeAndLadder.java
+++ src/com/bridgelabz/SAL/SnakeAndLadder.java
@@ -6,24 +6,32 @@ public class SnakeAndLadder {
 		int game_set[] = new int[100];
 		int position=0,dice_result,pos_result,new_pos;
 		System.out.println("Welcome to Snake And Ladder Game");
-		dice_result = (int) (Math.random()*(max_dice - min_dice + 1) + min_dice);
-		System.out.println("Your number after rolling the dice :- " + dice_result);
-		pos_result = (int) (Math.random()*(pos_max - pos_min + 1) + pos_min);
-		if(pos_result == 1) {
-			System.out.println("No Play");
-			new_pos = position;
-		}
-		else if(pos_result == 2){
-			System.out.println("Move ahead");
-			new_pos = position + dice_result;
-		}
-		else if(pos_result == 3){
-			if(position != 0) {
-				System.out.println("Move behind");
-			    new_pos = position + dice_result;
+		
+		while(position!=100) {
+			dice_result = (int) (Math.random()*(max_dice - min_dice + 1) + min_dice);
+			System.out.println("Your number after rolling the dice :- " + dice_result);
+			pos_result = (int) (Math.random()*(pos_max - pos_min + 1) + pos_min);
+			if(pos_result == 1) {
+				System.out.println("No Play");
+				new_pos = position;
 			}
-			else {
-				System.out.println("your are on start play again");
+			else if(pos_result == 2){
+				System.out.println("Move ahead");
+				new_pos = position + dice_result;
+			}
+			else if(pos_result == 3){
+				if(position != 0) {
+					System.out.println("Move behind");
+				    new_pos = position + dice_result;
+				    if(new_pos<0) {
+				    	System.out.println("Start again");
+				    	position = 0;
+				    }
+				}
+				else {
+					System.out.println("your are on start play again");
+					
+				}
 			}
 		}
 	}
