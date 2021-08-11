cd85c712a02cd4a4f881a9b8002f1d94a229bf7f
Prakash-16 <goudprakash58@gmail.com>
2021-08-11 10:16:01 +0530

    [Prakash] - Welcome message added

---
+++ src/com/bridgelabz/SAL/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.SAL;
+
+public class SnakeAndLadder {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Snake And Ladder Game");
+		
+		
+	}
+
+}
