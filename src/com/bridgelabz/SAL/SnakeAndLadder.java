03eac23615e95893ad15c56bf2377cd098380d9f
Prakash-16 <goudprakash58@gmail.com>
2021-08-11 10:18:48 +0530

    [Prakash] - Added Rolling Dice feature

--- src/com/bridgelabz/SAL/SnakeAndLadder.java
+++ src/com/bridgelabz/SAL/SnakeAndLadder.java
@@ -1,11 +1,13 @@
 package com.bridgelabz.SAL;
 
 public class SnakeAndLadder {
-	
+	public static final int max_dice = 6, min_dice = 1;
 	public static void main(String[] args) {
-		
+		int game_set[] = new int[100];
+		int position=0,dice_result;
 		System.out.println("Welcome to Snake And Ladder Game");
-		
+		dice_result = (int) (Math.random()*(max_dice - min_dice + 1) + min_dice);
+		System.out.println("Your number after rolling the dice :- " + dice_result);
 		
 	}
 
